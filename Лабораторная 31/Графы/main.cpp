6b962288b9b4f4b39782e2374f8a9020d5426978
RadmirMinimullin111 <63593801+RadmirMinimullin111@users.noreply.github.com>
2020-06-14 16:37:34 +0500

    Add files via upload

---
+++ Лабораторная 31/Графы/main.cpp
@@ -0,0 +1,26 @@
+#include "Head.h"
+#include "include/freeglut.h"
+#include "include/glut.h"
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	glutInit(&argc, argv);
+
+	graph = makeGraph();
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(900, 720);
+	glutCreateWindow("ֳנאפû");
+	WinW = glutGet(GLUT_WINDOW_WIDTH);
+	WinH = glutGet(GLUT_WINDOW_HEIGHT);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+
+	return 0;
+}
